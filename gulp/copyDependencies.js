6faed043f9ad4d8032cc6c2386463b8045404168
Olena Tsybulska <tsybulskayallena@gmail.com>
2021-01-06 17:44:15 +0200

    Add modal, update form

--- gulp/copyDependencies.js
+++ gulp/copyDependencies.js
@@ -5,10 +5,10 @@ const terser = require('gulp-terser')
 
 module.exports = function copyDependencies() {
     return gulp.src([
-            'node_modules/svg4everybody/dist/svg4everybody.legacy.min.js',
             'node_modules/jquery/dist/jquery.min.js',
+            '#src/scripts/jquery.spincrement.min.js',
             'node_modules/bootstrap/dist/js/bootstrap.bundle.min.js',
-            '#src/scripts/jquery.spincrement.min.js'
+            'node_modules/svg4everybody/dist/svg4everybody.legacy.min.js',
         ])
         .pipe(concat('libs.min.js'))
         //.pipe(terser())
